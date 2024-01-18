6a9641a224a1047c0b0332c7fd5f916d13c8e47d
AcrylicShrimp <led789zxpp@naver.com>
2024-01-18 09:40:07 +0900

    feat: add margin to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <main class="max-w-5xl mx-auto px-4 py-8">
   <slot />
 </main>
-<footer>
+<footer class="mt-8 md:mt-16">
   <div class="max-w-5xl mx-auto px-4 py-8 text-center text-gray-300">
     <p class="text-sm">&copy; {dayjs().format('2019-YYYY')} AcrylicShrimp. All rights reserved.</p>
   </div>
