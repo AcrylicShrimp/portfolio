9e8dcca1346af7b13eea42e8f3a5264f4f535eb5
AcrylicShrimp <led789zxpp@naver.com>
2024-01-18 09:39:55 +0900

    fix: override active css

--- src/routes/FilterTabButton.svelte
+++ src/routes/FilterTabButton.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <button
-  class={`m-2 md:m-4 w-24 h-8 text-xs md:text-base md:w-28 md:h-8 border border-antd-grey-1 text-antd-grey-1 hover:border-white hover:text-white transition-colors duration-300 ${active ? 'border-antd-gold text-antd-gold hover:border-antd-gold hover:text-antd-gold underline' : ''}`}
+  class={`m-2 md:m-4 w-24 h-8 text-xs md:text-base md:w-28 md:h-8 border transition-colors duration-300 ${active ? 'border-antd-gold text-antd-gold hover:border-antd-gold hover:text-antd-gold underline' : 'border-antd-grey-1 text-antd-grey-1 hover:border-white hover:text-white'}`}
   on:click><slot /></button
 >
