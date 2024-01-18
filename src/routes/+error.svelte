93fb11ced10571b71d6274b2ea4f80b832022349
AcrylicShrimp <led789zxpp@naver.com>
2024-01-19 08:02:13 +0900

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { base } from '$app/paths';
+</script>
+
+<h1 class="text-4xl">Project Portfolio</h1>
+<hr class="mt-2" />
+<section class="mt-16">
+  <h1 class="text-xl text-center md:text-2xl md:text-start">없는 페이지</h1>
+  <p class="mt-8 text-sm md:text-base text-center md:text-start">
+    주소가 잘못된 것 같습니다. 올바른 주소인지 한번 더 확인해주세요!
+  </p>
+  <p class="mt-8 text-lg text-center text-antd-gold">
+    <a href="{base}/">돌아가기</a>
+  </p>
+</section>
