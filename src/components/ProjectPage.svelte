c549d5e8bd3d934044f39c593f8554e6d55498f8
AcrylicShrimp <led789zxpp@naver.com>
2024-01-19 08:23:51 +0900

    feat: add r3d page

---
+++ src/components/ProjectPage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { ProjectInfo } from '@lib/ProjectInfo';
+
+  export let info: ProjectInfo;
+</script>
+
+<h1 class="text-4xl">Project Portfolio</h1>
+<hr class="mt-2" />
+<section class="mt-16">
+  <h1 class="text-xl text-center md:text-2xl md:text-start">{info.name}</h1>
+  <div class="mt-4">
+    <slot />
+  </div>
+</section>
---
+++ src/routes/projects/r3d/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import ProjectPage from '@components/ProjectPage.svelte';
+  import { r3d } from '..';
+</script>
+
+<ProjectPage info={r3d}>
+  <div>r3d</div>
+</ProjectPage>
